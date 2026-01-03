dfd8db347bb83872c81057522dd659e625f147e5
Vijay kanth mekala <84648201+VijayKanthMekala@users.noreply.github.com>
2026-01-03 14:20:01 +0530

    Add files via upload

---
+++ triangularPattern1.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a number :"))
+for i in range(1,n+1):
+    for j in range(i,n+1):
+     print("*",end=" ")
+    print()
+
+
+#     n = 3
+#     for i in range(n):
+#     for j in range(n - i):
+#     print('*', end=' ')
+#     print()
